67373526e054316d137e2687de4d950b480b027b
Eugene Kotov <mrkev2112@mail.ru>
2020-09-26 00:59:38 +0600

    add order in modules

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,15 @@ import Vuex from 'vuex'
 
 import * as shop from '@/store/modules/shop.js'
 import * as cart from '@/store/modules/cart.js'
+import * as order from '@/store/modules/order.js'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     shop,
-    cart
+    cart,
+    order
   },
   state: {}
 })
