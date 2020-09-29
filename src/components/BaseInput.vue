c0eba6d61a08565053b211f9c979dc2eab0104cb
Eugene Kotov <mrkev2112@mail.ru>
2020-09-29 17:46:46 +0600

    create BaseInput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div class="">
+    <label v-if="label"> {{ label }} </label>
+    <input :value="value" @input="updateValue" v-on="listeners" :v-bind="$attrs">
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      defaul: ''
+    },
+    value: [String, Number]
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  },
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: this.updateValue 
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped >
+
+</style>
