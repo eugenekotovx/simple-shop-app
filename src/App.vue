d000e5b77b51ae2be5e116dfa3dbfbd93d2835d9
Eugene Kotov <mrkev2112@mail.ru>
2020-10-01 23:35:54 +0600

    replace cart to app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app" class="container">
     <NavBar/>
+    <Cart/>
     <router-view/>
   </div>
 </template>
 
+<script type="text/javascript">
 import NavBar from '@/components/NavBar'
+import Cart from '@/components/Cart'
+export default {
+  components: {
     NavBar,
+    Cart
+  }
+}
+</script>
 <style lang="scss">
+
 body {
     background-color: #bbe1fa;
 }
@@ -15,29 +25,24 @@ body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-weight: bold;
   color: #1a1a2e;
 }
 .container {
   max-width: 600px;
   margin: 0 auto;
 }
-#nav {
-  padding: 30px 0px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  a {
-    font-weight: bold;
-    color: #1a1a2e;
-    text-decoration: none;
-    padding: 5px 10px;
-    &.router-link-exact-active {
-      background-color: #0f4c75;
-      border-radius: 10px;
-      color: #bbe1fa;
-      transition: .2s;
-    }
+.link {
+  text-decoration: none;
+  padding: 5px 10px;
+  border-radius: 10px;
+  font-weight: bold;
+  font-size: 16px;
+  color: #1a1a2e;
+  &:hover {
+    background-color: #0f4c75;
+    color: #bbe1fa;
+    transition: .2s;
   }
 }
 </style>
