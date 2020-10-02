00c21803ab7e1eb1261eb3e390b7aea4295d51fe
Eugene Kotov <mrkev2112@mail.ru>
2020-10-02 22:23:34 +0600

    import user module

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,15 @@ import Vuex from 'vuex'
 import * as shop from '@/store/modules/shop.js'
 import * as cart from '@/store/modules/cart.js'
 import * as order from '@/store/modules/order.js'
-
+import * as user from '@/store/modules/user.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     shop,
     cart,
-    order
+    order,
+    user
   },
   state: {}
 })
