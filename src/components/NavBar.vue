948d81bc9d68d031bee001f3dff9220ae3692535
Eugene Kotov <mrkev2112@mail.ru>
2020-10-04 05:22:27 +0600

    add styles

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,25 +2,19 @@
   <div id="nav">
     <router-link class="link" to="/shop">Shop</router-link>
     <router-link class="link" to="/cart">Cart</router-link>
-    <RegistrationModal/>
   </div>
 </template>
 
 <script>
-import RegistrationModal from '@/views/RegistrationModal'
 export default {
-  components: {
-    RegistrationModal
-  }
 }
 </script>
 
 <style lang="scss" scoped >
-  #nav {
-    padding: 30px 0px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-  }
+ #nav {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+ }
 </style>
