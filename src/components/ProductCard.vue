c356e9984a0d6a21753e633362ee22c5f0d68bae
Eugene Kotov <mrkev2112@mail.ru>
2020-10-10 23:43:18 +0700

    Product controls moved to a separate component

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -8,50 +8,18 @@
         <h3> {{ product.name }}</h3>
         <h3> PRICE: {{ product.price + '$' }}</h3>
       </div>
-
-        <img class="product__img" :src="require(`@/assets/img/` + product.name.split(' ').join('_') + '.png')" alt="">
+      <img class="product__img" :src="require(`@/assets/img/` + product.name.split(' ').join('_') + '.png')" alt="">
     </div>
    </router-link>
-   <div class="product__group">
-       <BaseButton
-       buttonClass="button-active"
-       v-if="!product.count"
-       @click.once="addProduct(product)">
-       <BaseIcon
-       :width="37"
-       :height="37"
-       :name="'cart'"
-       />
-       <span class="button__text">
-         Add product in cart
-       </span>
-      </BaseButton>
-      <div class="product__controls" v-if="product.count >= 1">
-        <BaseButton
-        type="button"
-        buttonClass="button-active"
-        @click="decrementCount(product)"
-        >-</BaseButton>
-        <BaseButton
-        type="button"
-        buttonClass="button-active"
-        @click="incrementCount(product)"
-        >+</BaseButton>
-      </div>
-     <template>
-       <span v-if="product.total !== 0 & product.count >= 1" class="cart__counter">
-         in cart: <span>{{product.count}} </span>
-       </span>
-       <span  v-else-if="product.total == 0" class="cart__counter sold-out"> SOLD OUT </span>
-     </template>
-   </div>
+   <ProductControls :product="product"/>
   </div>
 </template>
 
 <script>
-import {mapActions} from 'vuex'
+import ProductControls from '@/components/ProductControls'
 export default {
   components: {
+    ProductControls
   },
   props: {
     product: {
@@ -62,25 +30,14 @@ export default {
       type: String,
     }
   },
-  methods: {
-    ...mapActions(['addProduct', 'incrementCount','decrementCount'])
-    }
-  }
+}
 
 </script>
 
 <style lang="scss" scoped >
-  .cart__counter {
-    max-width: 150px;
-    width: 100%;
-    text-align: center;
-  }
   .product__img {
     width: 150px;
     height: 150px;
-  }
-  .sold-out {
-    color: red;
   }
   .product{
    &__card {
@@ -94,20 +51,11 @@ export default {
      }
      margin-bottom: 10px;
    }
-   &__group {
-     display: flex;
-     flex-direction: row;
-     justify-content: space-between;
-     align-items: center;
-   }
    &__wrapper {
      display: flex;
      flex-direction: row;
      justify-content: space-between;
      align-items:  center;
-   }
-   &__controls {
-     display: flex;
    }
   }
 </style>
---
+++ src/components/ProductControls.vue
@@ -0,0 +1,85 @@
+<template lang="html">
+  <div class="controls__group">
+      <BaseButton
+        buttonClass="button-active"
+        v-if="!itemInCart.count"
+        @click.once="addProduct(product)">
+      <BaseIcon
+        :width="37"
+        :height="37"
+        :name="'cart'"
+      />
+      <span class="button__text">
+        Add product in cart
+      </span>
+     </BaseButton>
+   <div class="controls"
+    v-if="product.total !== 0 && itemInCart.count >= 1">
+     <BaseButton
+       type="button"
+       buttonClass="button-active"
+       @click="decrementCount(product)"
+     >-</BaseButton>
+     <BaseButton
+       type="button"
+       buttonClass="button-active"
+       @click="incrementCount(product)"
+     >+</BaseButton>
+   </div>
+    <span
+      v-if="itemInCart.count >= 1"
+      class="cart__counter"
+    >
+      in cart: <span>{{itemInCart.count}} </span>
+    </span>
+    <span
+      v-if="itemInCart.total == 0"
+      class="cart__counter sold-out"
+    > SOLD OUT </span>
+  </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    itemInCart() {
+      var item = this.$store.state.cart.cart.find(item => item.id == this.product.id)
+      if (item) {
+       return item
+      } else {
+       return this.product
+      }
+    }
+  },
+  methods: {
+    ...mapActions(['addProduct', 'incrementCount','decrementCount']),
+  }
+}
+</script>
+
+<style lang="scss" scoped >
+.cart__counter {
+  max-width: 150px;
+  width: 100%;
+  text-align: center;
+}
+.controls {
+  display: flex;
+  &__group {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+.sold-out {
+  color: red;
+}
+</style>
